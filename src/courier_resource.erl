9cc5d71d89c2cfd70d420cc43af0fbce547b2d72
Ryan Denby <ryan.543@live.co.uk>
2022-11-24 00:08:14 +0000

    patterns for uris

--- src/courier_resource.erl
+++ src/courier_resource.erl
@@ -12,16 +12,30 @@
 %% API
 -export([]).
 
--type resource()  :: {URI         :: binary(),
-                      Handler     :: module(),
-                      HandlerArgs :: [term()]}.
+-type resource() :: {UriName     :: binary(),
+                     Uri         :: iodata(),
+                     Handler     :: module(),
+                     HandlerArgs :: [term()]}.
 -type resources() :: [resource()].
 -export_type([resources/0]).
+
+-type uri_pattern() :: {UriPattern     :: re:mp(),
+                        UriPatternKeys ::{namelist, [binary()]}}.
 
 %%%-------------------------------------------------------------------
 %%% API
 %%%-------------------------------------------------------------------
 
+
 %%%-------------------------------------------------------------------
 %%% Internal functions
 %%%-------------------------------------------------------------------
+
+create_uri_pattern(UriRegex) ->
+  {ok, UriPattern}           = re:compile(UriRegex),
+  {namelist, UriPatternKeys} = re:inspect(UriPattern, namelist),
+  {UriPattern, UriPatternKeys}.
+
+get_uri_variable_map({UriPattern, UriPatternKeys}) ->
+  {match, UriVars} = re:run(UriPattern, [global, trim_all]),
+  maps:from_list(lists:zip(UriPatternKeys, UriVars)).
